4fa8e11def78c6c9b7d10183953a7d1a247eeb12
Sujal Thakkar <sujalthakkar153@gmail.com>
2025-04-06 22:20:51 +0530

    Add treasure hunt page with styles and functionality; update coding challenge image

---
+++ competition-instructions/treasure-hunt.js
@@ -0,0 +1,64 @@
+
+function toggleMenu() {
+    const navMenu = document.querySelector('.nav-menu');
+    const openIcon = document.querySelector('.open-icon');
+    const closeIcon = document.querySelector('.close-icon');
+
+    navMenu.classList.toggle('active');
+
+    if (navMenu.classList.contains('active')) {
+        openIcon.style.display = 'none';
+        closeIcon.style.display = 'block';
+    } else {
+        openIcon.style.display = 'block';
+        closeIcon.style.display = 'none';
+    }
+}
+
+let lastScrollPosition = 0;
+const header = document.querySelector('header');
+
+window.addEventListener('scroll', () => {
+    const currentScrollPosition = window.scrollY;
+
+    if (currentScrollPosition > 0) {
+        header.classList.add('scrolled');
+    } else {
+        header.classList.remove('scrolled');
+    }
+
+    if (currentScrollPosition > lastScrollPosition) {
+        header.classList.add('header-hidden');
+    } else {
+        header.classList.remove('header-hidden');
+    }
+
+    if (currentScrollPosition === 0) {
+        header.classList.remove('header-hidden');
+    }
+
+    lastScrollPosition = currentScrollPosition;
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+    const goToTopBtn = document.getElementById('goToTopBtn');
+
+    window.addEventListener('scroll', function () {
+        const heightToShow = 20;
+        const scrollPosition = window.scrollY || document.documentElement.scrollTop;
+
+        if (scrollPosition > heightToShow) {
+            goToTopBtn.style.display = 'flex';
+        } else {
+            goToTopBtn.style.display = 'none';
+        }
+    });
+
+    goToTopBtn.addEventListener('click', function () {
+        window.scrollTo({
+            top: 0,
+            left: 0,
+            behavior: 'smooth'
+        });
+    });
+});
